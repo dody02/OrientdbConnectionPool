9237211389ab9296496679e08f16131fa76845cd
dody <dy0222@163.com>
2021-09-07 14:43:34 +0800

    push

---
+++ src/main/java/net/sf/orientdbpool/CurrentConnectionDisableException.java
@@ -0,0 +1,15 @@
+package net.sf.orientdbpool;
+
+/**
+ * 当前连接不可使用异常
+ * @author asus
+ *
+ */
+public class CurrentConnectionDisableException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
